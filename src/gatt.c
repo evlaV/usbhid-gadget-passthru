3f89499f1d2950c9e1e5d7b6f1da87d2d2f96840
Vicki Pfau <vi@endrift.com>
2022-12-22 17:10:53 -0800

    Basic GATT descriptor bringup

--- src/gatt.c
+++ src/gatt.c
@@ -8,6 +8,10 @@
 #include <stdlib.h>
 
 static int read_characteristic(sd_bus_message* m, void *userdata, sd_bus_error* error);
+static int read_descriptor(sd_bus_message* m, void *userdata, sd_bus_error* error);
+
+static int read_service_path(sd_bus*, const char*, const char*, const char*, sd_bus_message* reply, void* userdata, sd_bus_error*);
+static int read_characteristic_path(sd_bus*, const char*, const char*, const char*, sd_bus_message* reply, void* userdata, sd_bus_error*);
 
 static const sd_bus_vtable gatt_service[] = {
 	SD_BUS_VTABLE_START(0),
@@ -19,20 +23,25 @@ static const sd_bus_vtable gatt_service[] = {
 static const sd_bus_vtable gatt_characteristic[] = {
 	SD_BUS_VTABLE_START(0),
 	SD_BUS_PROPERTY("UUID", "s", read_string, offsetof(struct GattCharacteristic, uuid), SD_BUS_VTABLE_PROPERTY_CONST),
-	SD_BUS_PROPERTY("Service", "o", read_object_indirect, offsetof(struct GattCharacteristic, service), SD_BUS_VTABLE_PROPERTY_CONST),
+	SD_BUS_PROPERTY("Service", "o", read_service_path, 0, SD_BUS_VTABLE_PROPERTY_CONST),
 	SD_BUS_PROPERTY("Flags", "as", read_string_array, offsetof(struct GattCharacteristic, flags), SD_BUS_VTABLE_PROPERTY_CONST),
 	SD_BUS_PROPERTY("MTU", "q", read_uint16, offsetof(struct GattCharacteristic, mtu), SD_BUS_VTABLE_PROPERTY_CONST),
 	SD_BUS_METHOD_WITH_ARGS("ReadValue", "a{sv}", "ay", read_characteristic, 0),
 	SD_BUS_VTABLE_END
 };
 
-static int read_characteristic(sd_bus_message* m, void *userdata, sd_bus_error* error) {
-	struct GattCharacteristic* characteristic = userdata;
+static const sd_bus_vtable gatt_descriptor[] = {
+	SD_BUS_VTABLE_START(0),
+	SD_BUS_PROPERTY("UUID", "s", read_string, offsetof(struct GattDescriptor, uuid), SD_BUS_VTABLE_PROPERTY_CONST),
+	SD_BUS_PROPERTY("Characteristic", "o", read_characteristic_path, 0, SD_BUS_VTABLE_PROPERTY_CONST),
+	SD_BUS_PROPERTY("Flags", "as", read_string_array, offsetof(struct GattDescriptor, flags), SD_BUS_VTABLE_PROPERTY_CONST),
+	SD_BUS_METHOD_WITH_ARGS("ReadValue", "a{sv}", "ay", read_descriptor, 0),
+	SD_BUS_VTABLE_END
+};
+
+static int parse_flags(sd_bus_message* m, size_t* offset, size_t* length, const char* objtype, sd_bus_error* error) {
 	const char* opt;
 	int res;
-	size_t offset = 0;
-	size_t length = characteristic->size;
-	sd_bus_message* reply;
 
 	res = sd_bus_message_enter_container(m, 'a', "{sv}");
 	if (res < 0) {
@@ -61,11 +70,11 @@ static int read_characteristic(sd_bus_message* m, void *userdata, sd_bus_error* 
 				return res;
 			}
 
-			if (read_offset > length) {
-				return sd_bus_error_setf(error, "org.bluez.Error.InvalidOffset", "Requested offset %u exceeds characteristic size %zu", read_offset, length);
+			if (read_offset > *length) {
+				return sd_bus_error_setf(error, "org.bluez.Error.InvalidOffset", "Requested offset %u exceeds %s size %zu", read_offset, objtype, *length);
 			}
-			offset = read_offset;
-			length -= read_offset;
+			*offset = read_offset;
+			*length -= read_offset;
 		}
 
 		res = sd_bus_message_exit_container(m);
@@ -79,6 +88,20 @@ static int read_characteristic(sd_bus_message* m, void *userdata, sd_bus_error* 
 
 	res = sd_bus_message_exit_container(m);
 	if (res < 0) {
+		return res;
+	}
+	return 0;
+}
+
+static int read_characteristic(sd_bus_message* m, void *userdata, sd_bus_error* error) {
+	struct GattCharacteristic* characteristic = userdata;
+	int res;
+	size_t offset = 0;
+	size_t length = characteristic->size;
+	sd_bus_message* reply;
+
+	res = parse_flags(m, &offset, &length, "characteristic", error);
+	if (res < 0 || sd_bus_error_is_set(error)) {
 		return res;
 	}
 
@@ -95,6 +118,52 @@ static int read_characteristic(sd_bus_message* m, void *userdata, sd_bus_error* 
 	return sd_bus_message_send(reply);
 }
 
+static int read_descriptor(sd_bus_message* m, void *userdata, sd_bus_error* error) {
+	struct GattDescriptor* descriptor = userdata;
+	int res;
+	size_t offset = 0;
+	size_t length = descriptor->size;
+	sd_bus_message* reply;
+
+	res = parse_flags(m, &offset, &length, "descriptor", error);
+	if (res < 0 || sd_bus_error_is_set(error)) {
+		return res;
+	}
+
+	res = sd_bus_message_new_method_return(m, &reply);
+	if (res < 0) {
+		return res;
+	}
+
+	res = sd_bus_message_append_array(reply, 'y', &((uint8_t*) descriptor->data)[offset], length);
+	if (res < 0) {
+		return res;
+	}
+
+	return sd_bus_message_send(reply);
+}
+
+static int read_service_path(sd_bus*, const char*, const char*, const char*, sd_bus_message* reply, void* userdata, sd_bus_error*) {
+	struct GattCharacteristic* characteristic = userdata;
+	struct GattService* service = characteristic->service;
+	return sd_bus_message_append(reply, "o", service->path);
+}
+
+static int read_characteristic_path(sd_bus*, const char*, const char*, const char*, sd_bus_message* reply, void* userdata, sd_bus_error*) {
+	struct GattDescriptor* descriptor = userdata;
+	struct GattCharacteristic* characteristic = descriptor->characteristic;
+	struct GattService* service = characteristic->service;
+	char path[PATH_MAX] = {0};
+	size_t i;
+	for (i = 0; i < service->nCharacteristics; ++i) {
+		if (characteristic == service->characteristics[i]) {
+			break;
+		}
+	}
+	snprintf(path, sizeof(path) - 1, "%s/char%04zx", service->path, i);
+	return sd_bus_message_append(reply, "o", path);
+}
+
 void gatt_service_create(struct GattService* service, const char* uuid, const char* path) {
 	memset(service, 0, sizeof(*service));
 	strncpy(service->uuid, uuid, sizeof(service->uuid) - 1);
@@ -103,7 +172,7 @@ void gatt_service_create(struct GattService* service, const char* uuid, const ch
 }
 
 int gatt_service_register(struct GattService* service, sd_bus* bus) {
-	size_t i;
+	size_t i, j;
 	int res = sd_bus_add_object_vtable(bus, &service->slot, service->path,
 		"org.bluez.GattService1", gatt_service, service);
 	if (res < 0) {
@@ -112,22 +181,31 @@ int gatt_service_register(struct GattService* service, sd_bus* bus) {
 
 	for (i = 0; i < service->nCharacteristics; ++i) {
 		char path[PATH_MAX] = {0};
+		struct GattCharacteristic* characteristic = service->characteristics[i];
 		snprintf(path, sizeof(path) - 1, "%s/char%04zx", service->path, i);
-		res = sd_bus_add_object_vtable(bus, &service->characteristics[i]->slot, path,
-			 "org.bluez.GattCharacteristic1", gatt_characteristic, service->characteristics[i]);
+		res = sd_bus_add_object_vtable(bus, &characteristic->slot, path,
+			 "org.bluez.GattCharacteristic1", gatt_characteristic, characteristic);
 		if (res < 0) {
-			break;
+			return res;
+		}
+		for (j = 0; j < characteristic->nDescriptors; ++j) {
+			snprintf(path, sizeof(path) - 1, "%s/char%04zx/desc%04zx", service->path, i, j);
+			res = sd_bus_add_object_vtable(bus, &characteristic->descriptors[j]->slot, path,
+				 "org.bluez.GattDescriptor1", gatt_descriptor, characteristic->descriptors[j]);
+			if (res < 0) {
+				return res;
+			}
 		}
 	}
-	return res;
+	return 0;
 };
 
 void gatt_characteristic_create(struct GattCharacteristic* characteristic, const char* uuid, struct GattService* parent) {
 	memset(characteristic, 0, sizeof(*characteristic));
 	strncpy(characteristic->uuid, uuid, sizeof(characteristic->uuid) - 1);
-	characteristic->service = parent->path;
+	characteristic->service = parent;
 
-	if (parent->nCharacteristics == MAX_CHAR) {
+	if (parent->nCharacteristics == MAX_GATT_CHAR) {
 		abort();
 	}
 
@@ -135,3 +213,15 @@ void gatt_characteristic_create(struct GattCharacteristic* characteristic, const
 	++parent->nCharacteristics;
 }
 
+void gatt_descriptor_create(struct GattDescriptor* descriptor, const char* uuid, struct GattCharacteristic* parent) {
+	memset(descriptor, 0, sizeof(*descriptor));
+	strncpy(descriptor->uuid, uuid, sizeof(descriptor->uuid) - 1);
+	descriptor->characteristic = parent;
+
+	if (parent->nDescriptors == MAX_GATT_DESC) {
+		abort();
+	}
+
+	parent->descriptors[parent->nDescriptors] = descriptor;
+	++parent->nDescriptors;
+}
